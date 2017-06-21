afe6200c9ab911d38923d8d5ed9fba2b84b3f01d
J Lyons <joshua.lyons@byu.edu>
2017-06-21 15:32:33 -0600

    Created portfolio

---
+++ portfolio/js/content.js
@@ -0,0 +1,95 @@
+content = [
+  {
+    "name": "Fill In The Gap",
+    "sub": "Nonprofit website",
+    "img": "fitg_home.PNG",
+    "description": "This website was created for the nonprofit Fill In The Gap Inc. and is a pretty simple static website. It is actually just a Github Pages website with a CNAME to a custom domain, which makes it virtually free for the nonprofit to host, they simply pay the yearly domain registration fee.",
+    "written": [
+      "html",
+      "css",
+      "jQuery/javascript",
+      "A little bit of Visual basics for the form submition to a Google Sheet",
+    ],
+    "links":[
+      {
+        "name": "FITG Home",
+        "href": "http://www.fillinthegapproject.org/index.html"
+      },
+      {
+        "name": "FITG Application Page",
+        "href": "http://www.fillinthegapproject.org/apply.html"
+      }
+    ]
+  },
+  {
+    "name": "Eye on the Time",
+    "sub": "Segmented presentation timer",
+    "img": "eott_home.png",
+    "description": "Allows users to create timers with custom titles and lengths, save the timers, and revisit them for later use. The purpose of these timers is to keep a presentation on track by timing each individual segment of the lecture.",
+    "written": [
+      "Node JS/Express",
+      "html",
+      "css",
+      "jQuery/javascript",
+      "MongoDB backend"
+    ],
+    "links":[
+      {
+        "name": "Unavailable",
+        "href": "#"
+      }
+    ]
+  },
+  {
+    "name": "BYU Radio S-Link",
+    "sub": "Radio track information manager",
+    "img": "slink_home.png",
+    "description": "Management page for controling track information on BYU Radio. A python module receives current track information and sends it to Dish and to SiriusXM. This is wrapped in a python Twisted server that this webpage interacts with. This is all behind PHP authentication to ensure security of reseting the Dish and SiriusXM modules, and updating track information.",
+    "written": [
+      "python",
+      "html",
+      "css",
+      "jQuery/javascript",
+    ],
+    "links":[
+      {
+        "name": "Unavailable",
+        "href": "#"
+      }
+    ]
+  },
+  {
+    "name": "Xfer",
+    "sub": "File tranfer manager",
+    "img": "xfer_home.png",
+    "description": "A python backend watches drop folders, sends new files to an agent that transfers them to a determined destination. This is all warpped in a python twisted webserver/API. The management page allows monitoring of agents, watchers, and current running jobs.",
+    "written": [
+      "python",
+      "ASP.NET"
+    ],
+    "links":[
+      {
+        "name": "Unavailable",
+        "href": "#"
+      }
+    ]
+  },
+  {
+    "name": "Copycat",
+    "sub": "DNS sync manager",
+    "img": "copycat_home.png",
+    "description": "A python module watches a database tied to an IP management service and copies changes over to production DNS servers. This module is wrapped in a python server that manages the copycat agents and various filters on DNS rules.",
+    "written": [
+      "python",
+      "html",
+      "css",
+      "jQuery/javascript",
+    ],
+    "links":[
+      {
+        "name": "Unavailable",
+        "href": ""
+      }
+    ]
+  }
+];
